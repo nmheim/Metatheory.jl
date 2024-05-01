45ee415abaa457edc2d2ed78288e724c67e32034
Niklas Heim <heim.niklas@gmail.com>
2024-05-01 13:35:19 +0200

    include all mt benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -20,79 +20,79 @@ include(joinpath(dirname(pathof(Metatheory)), "../examples/while_superinterprete
 SUITE["egraph"] = BenchmarkGroup(["egraphs"])
 SUITE["egraph"]["constructor"] = @benchmarkable EGraph()
 
-# rand_letter() = Symbol(rand('a':'z'))
-# 
-# function nested_expr(level)
-#   if level > 0
-#     :(($(rand_letter()))($(rand_letter())) + $(rand_letter()) + $(rand(1:100)) * $(nested_expr(level - 1)))
-#   else
-#     rand_letter()
-#   end
-# end
-# 
-# SUITE["egraph"]["addexpr"] = @benchmarkable EGraph($(nested_expr(2000)))
-# 
-# 
-# # ==================================================================
-# 
-# SUITE["basic_maths"] = BenchmarkGroup(["egraphs"])
-# 
-# 
-# simpl1_math = :(a + b + (0 * c) + d)
-# SUITE["basic_maths"]["simpl1"] = @benchmarkable (@assert :(a + b + d) == simplify(
-#   $simpl1_math,
-#   $maths_theory,
-#   $(SaturationParams(; timer = false)),
-#   postprocess_maths,
-# ))
-# 
-# simpl2_math = :(0 + (1 * foo) * 0 + (a * 0) + a)
-# SUITE["basic_maths"]["simpl2"] =
-#   @benchmarkable (@assert :a == simplify($simpl2_math, $maths_theory, $(SaturationParams()), postprocess_maths))
-# 
-# 
-# # ==================================================================
-# 
-# SUITE["prop_logic"] = BenchmarkGroup(["egraph", "logic"])
-# 
-# ex_orig = :(((p ⟹ q) && (r ⟹ s) && (p || r)) ⟹ (q || s))
-# ex_logic = rewrite(ex_orig, impl)
-# 
-# SUITE["prop_logic"]["rewrite"] = @benchmarkable rewrite($ex_orig, $impl)
-# SUITE["prop_logic"]["prove1"] = @benchmarkable (@assert prove($propositional_logic_theory, $ex_logic, 3, 6))
-# 
-# ex_demorgan = :(!(p || q) == (!p && !q))
-# SUITE["prop_logic"]["demorgan"] = @benchmarkable (@assert prove($propositional_logic_theory, $ex_demorgan))
-# 
-# ex_frege = :((p ⟹ (q ⟹ r)) ⟹ ((p ⟹ q) ⟹ (p ⟹ r)))
-# SUITE["prop_logic"]["freges_theorem"] = @benchmarkable (@assert prove($propositional_logic_theory, $ex_frege))
-# 
-# # ==================================================================
-# 
-# SUITE["calc_logic"] = BenchmarkGroup(["egraph", "logic"])
-# 
-# SUITE["calc_logic"]["demorgan"] = @benchmarkable (@assert prove($calculational_logic_theory, $ex_demorgan))
-# # SUITE["calc_logic"]["freges_theorem"] =
-# #   @benchmarkable (@assert prove($calculational_logic_theory, $ex_frege, 1, 10, 10000))
-# 
-# # ==================================================================
-# 
-# SUITE["while_superinterpreter"] = BenchmarkGroup(["egraph"])
-# 
-# exx = :((while x < 10
-#   x = x + 1
-# end;
-# x), $(Mem(:x => 3)))
-# 
-# function bench_while_superinterpreter(expr, expected)
-#   g = EGraph()
-#   id1 = addexpr!(g, expr)
-#   g.root = id1
-#   id2 = addexpr!(g, expected)
-#   goal = (g::EGraph) -> in_same_class(g, id1, id2)
-#   params = SaturationParams(timeout = 100, goal = goal, scheduler = Schedulers.SimpleScheduler)
-#   rep = saturate!(g, while_language, params)
-#   @assert expected == extract!(g, astsize)
-# end
-# 
-# SUITE["while_superinterpreter"]["while_10"] = @benchmarkable bench_while_superinterpreter($exx, 10)
+rand_letter() = Symbol(rand('a':'z'))
+
+function nested_expr(level)
+  if level > 0
+    :(($(rand_letter()))($(rand_letter())) + $(rand_letter()) + $(rand(1:100)) * $(nested_expr(level - 1)))
+  else
+    rand_letter()
+  end
+end
+
+SUITE["egraph"]["addexpr"] = @benchmarkable EGraph($(nested_expr(2000)))
+
+
+# ==================================================================
+
+SUITE["basic_maths"] = BenchmarkGroup(["egraphs"])
+
+
+simpl1_math = :(a + b + (0 * c) + d)
+SUITE["basic_maths"]["simpl1"] = @benchmarkable (@assert :(a + b + d) == simplify(
+  $simpl1_math,
+  $maths_theory,
+  $(SaturationParams(; timer = false)),
+  postprocess_maths,
+))
+
+simpl2_math = :(0 + (1 * foo) * 0 + (a * 0) + a)
+SUITE["basic_maths"]["simpl2"] =
+  @benchmarkable (@assert :a == simplify($simpl2_math, $maths_theory, $(SaturationParams()), postprocess_maths))
+
+
+# ==================================================================
+
+SUITE["prop_logic"] = BenchmarkGroup(["egraph", "logic"])
+
+ex_orig = :(((p ⟹ q) && (r ⟹ s) && (p || r)) ⟹ (q || s))
+ex_logic = rewrite(ex_orig, impl)
+
+SUITE["prop_logic"]["rewrite"] = @benchmarkable rewrite($ex_orig, $impl)
+SUITE["prop_logic"]["prove1"] = @benchmarkable (@assert prove($propositional_logic_theory, $ex_logic, 3, 6))
+
+ex_demorgan = :(!(p || q) == (!p && !q))
+SUITE["prop_logic"]["demorgan"] = @benchmarkable (@assert prove($propositional_logic_theory, $ex_demorgan))
+
+ex_frege = :((p ⟹ (q ⟹ r)) ⟹ ((p ⟹ q) ⟹ (p ⟹ r)))
+SUITE["prop_logic"]["freges_theorem"] = @benchmarkable (@assert prove($propositional_logic_theory, $ex_frege))
+
+# ==================================================================
+
+SUITE["calc_logic"] = BenchmarkGroup(["egraph", "logic"])
+
+SUITE["calc_logic"]["demorgan"] = @benchmarkable (@assert prove($calculational_logic_theory, $ex_demorgan))
+# SUITE["calc_logic"]["freges_theorem"] =
+#   @benchmarkable (@assert prove($calculational_logic_theory, $ex_frege, 1, 10, 10000))
+
+# ==================================================================
+
+SUITE["while_superinterpreter"] = BenchmarkGroup(["egraph"])
+
+exx = :((while x < 10
+  x = x + 1
+end;
+x), $(Mem(:x => 3)))
+
+function bench_while_superinterpreter(expr, expected)
+  g = EGraph()
+  id1 = addexpr!(g, expr)
+  g.root = id1
+  id2 = addexpr!(g, expected)
+  goal = (g::EGraph) -> in_same_class(g, id1, id2)
+  params = SaturationParams(timeout = 100, goal = goal, scheduler = Schedulers.SimpleScheduler)
+  rep = saturate!(g, while_language, params)
+  @assert expected == extract!(g, astsize)
+end
+
+SUITE["while_superinterpreter"]["while_10"] = @benchmarkable bench_while_superinterpreter($exx, 10)
